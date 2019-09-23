2de5070468a43e68999a3d50a6e6824eee83629f
Dave Menninger <dave.menninger@online-rewards.com>
2019-09-23 16:22:04 -0400

    minor fixes

--- lib/grex/client/util.ex
+++ lib/grex/client/util.ex
@@ -1,13 +1,15 @@
 defmodule Grex.Client.Util do
 
-  @base "http://www.goodreads.com"
+  @base "https://www.goodreads.com"
 
   defp body(response),   do: response.body
   defp parse(body),      do: body |> XmlNode.from_string
   defp uri(path, query), do: "#{@base}/#{path}.xml?#{URI.encode_query(query)}"
 
-  def get(path, query // []) do
-    uri(path, query) |> HTTPotion.get |> body |> parse
+  def get(path, query \\ []) do
+    uri(path, query)
+    |> HTTPotion.get #( [ ibrowse: [ proxy_host: "http://proxy.whapps.internal", proxy_port: 8192 ] ] )
+    |> body
+    |> parse
   end
-  
 end
