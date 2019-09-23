ac10be191dfac04ab9e31343714a717c5d68edd6
Dave Menninger <dave.menninger@online-rewards.com>
2019-09-23 15:54:35 -0400

    update versions

--- mix.exs
+++ mix.exs
@@ -4,20 +4,22 @@ defmodule Grex.Mixfile do
   def project do
     [ app: :grex,
       version: "0.0.1",
-      elixir: "~> 0.10.1",
       deps: deps ]
   end
 
   # Configuration for the OTP application
   def application do
-    []
+    [
+    ]
   end
 
   # Returns the list of dependencies in the format:
   # { :foobar, "~> 0.1", git: "https://github.com/elixir-lang/foobar.git" }
   defp deps do
-    [{:httpotion, github: "myfreeweb/httpotion"},
-     {:exactor,   github: "sasa1977/exactor"},
-     {:mock,      github: "jjh42/mock"}]
+    [
+      {:httpotion, "~> 3.1"},
+      {:exactor, "~> 2.2"},
+      {:mock, "~> 0.3.3"}
+    ]
   end
 end
