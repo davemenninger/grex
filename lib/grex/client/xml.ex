c4eb73d2f52e7bde24359d9b66afad8f5be25fd1
Dave Menninger <dave.menninger@online-rewards.com>
2019-09-23 16:21:06 -0400

    import Record

--- lib/grex/client/xml.ex
+++ lib/grex/client/xml.ex
@@ -1,46 +1,45 @@
 # https://gist.github.com/sasa1977/5967224
-
-defrecord :xmlAttribute, Record.extract(:xmlAttribute, from_lib: "xmerl/include/xmerl.hrl")
-defrecord :xmlText, Record.extract(:xmlText, from_lib: "xmerl/include/xmerl.hrl")
  
-defrecord XmlNode, element: nil do
-  def from_string(xml_string, options // [quiet: true]) do
+defmodule XmlNode do
+  require Record
+
+  Record.defrecord :xmlAttribute, Record.extract(:xmlAttribute, from_lib: "xmerl/include/xmerl.hrl")
+  Record.defrecord :xmlText, Record.extract(:xmlText, from_lib: "xmerl/include/xmerl.hrl")
+  Record.defrecord :xmlElement, Record.extract(:xmlElement, from_lib: "xmerl/include/xmerl.hrl")
+
+  def from_string(xml_string, options \\ [quiet: true]) do
     {doc, []} = 
       xml_string
       |> to_unicode_char_list
       |> :xmerl_scan.string(options)
  
-    from_element(doc)
+    doc
   end
- 
-  defp from_element(element), do: __MODULE__.new(element: element)
   
-  defmacrop empty_node, do: __MODULE__[element: nil]
- 
   def all(node, path) do
-    lc child_element inlist xpath(node, path) do 
-      from_element(child_element)
+    for child_element <- xpath(node, path) do 
+      child_element
     end
   end
  
-  def first(node, path), do: node |> xpath(path) |> take_one |> from_element
+  def first(node, path), do: node |> xpath(path) |> take_one
   defp take_one([head | _]), do: head
   defp take_one(_), do: nil
  
-  def node_name(empty_node()), do: nil
+  def node_name(nil), do: nil
   def node_name(node), do: elem(node.element, 1)
  
   def attr(node, name), do: node |> xpath('./@#{name}') |> extract_attr
-  defp extract_attr([:xmlAttribute[value: value]]), do: to_unicode_string(value)
+  defp extract_attr([xmlAttribute(value: value)]), do: to_unicode_string(value)
   defp extract_attr(_), do: nil
  
   def text(node), do: node |> xpath('./text()') |> extract_text
-  defp extract_text([:xmlText[value: value]]), do: to_unicode_string(value)
+  defp extract_text([xmlText(value: value)]), do: to_unicode_string(value)
   defp extract_text(_), do: nil
   
-  defp xpath(empty_node(), _), do: []
+  defp xpath(nil, _), do: []
   defp xpath(node, path) do
-    :xmerl_xpath.string(to_char_list(path), node.element)
+    :xmerl_xpath.string(to_charlist(path), node)
   end
  
   defp to_unicode_string(list) when is_list(list), do: :unicode.characters_to_binary(list)
@@ -49,6 +48,6 @@ defrecord XmlNode, element: nil do
   defp to_unicode_char_list(input) do
     input
     |> to_unicode_string
-    |> to_char_list
+    |> to_charlist
   end
 end
