ef03ea15252c51667e49f8cc3e9971d5afe88fe2
Dave Menninger <dave.menninger@online-rewards.com>
2019-09-23 21:04:31 -0400

    conditionally set opts based on env

--- lib/grex/client/util.ex
+++ lib/grex/client/util.ex
@@ -8,12 +8,23 @@ defmodule Grex.Client.Util do
 
   def get(path, query \\ []) do
     uri(path, query)
-    |> HTTPotion.get([ibrowse:
-      [
-        proxy_host: String.to_charlist(System.get_env("PROXY_HOST")),
-        proxy_port: String.to_integer(System.get_env("PROXY_PORT"))
-      ]])
+    |> HTTPotion.get(httpotion_opts())
     |> body
     |> parse
   end
+
+  defp httpotion_opts do
+    case System.get_env("PROXY_HOST") do
+      nil -> []
+      _ -> [
+          ibrowse:
+          [
+            proxy_host: String.to_charlist(System.get_env("PROXY_HOST")),
+            proxy_port: String.to_integer(System.get_env("PROXY_PORT"))
+          ]
+      ]
+    end
+  end
 end
+
+
